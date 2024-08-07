717529507117fbace3276cde2d79949f14602e85
Stefan Ruppe <ruppe@protonmail.com>
2024-08-07 11:33:00 +0000

    hello world get post

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    if request == "POST":
+        return HttpResponse("you must have posted smth")
+    else: return HttpResponse(request.method)
